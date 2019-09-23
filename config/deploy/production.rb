d979b6ad5c04e5b5a7a404e11d716edde906b0bc
Tatsuji Haga <blueandaqualine@gmail.com>
2019-09-23 20:41:07 +0900

    Add for deploy by capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '00.00.000.000', user: 'tatsuji', roles: %w{app db web} 
+server '13.112.108.182', user: 'tatsuji', roles: %w{app db web} 
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
 set :ssh_options, keys: '~/.ssh/toriposter_rsa_key'
