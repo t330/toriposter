ee333a9f53da3e106ba71f602a7affee8adaa79d
Tatsuji Haga <blueandaqualine@gmail.com>
2019-09-25 13:33:10 +0900

    Add for deploy by capistrano

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,31 @@
-app_path = File.expand_path('../../../', __FILE__)
+shared_path = "/var/www/rails/toriposter/shared/"
+current_path = "/var/www/rails/toriposter/current"
 
-worker_processes 1
-# currentを指定
-working_directory "#{app_path}/current"
+# ソケット経由で通信する
+# ここがcapistranoの設定と合致していないと失敗する
+listen File.expand_path('tmp/sockets/unicorn.sock', shared_path)
+pid File.expand_path('tmp/pids/unicorn.pid', shared_path)
 
-# それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
+# capistrano 用に RAILS_ROOT を指定
+working_directory current_path
+
+# ダウンタイムなくす
+preload_app true
+
+before_fork do |server, worker|
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', current_path)
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
