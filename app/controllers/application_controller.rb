eb54979a7bc903a721e1fd60d1faec75464d92f5
Tatsuji Haga <blueandaqualine@gmail.com>
2019-09-28 01:55:35 +0900

    I changed the way latitude and longitude information is inserted into DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :left_aside
+  before_action :right_aside
 
   protected
 
@@ -12,6 +13,10 @@ class ApplicationController < ActionController::Base
       @classifications = BirdList.all
       #@classifications = Classification.all.order('id ASC').limit(1)
       @bird = Post.where('name LIKE(?)', "%#{params[:bird]}%").order('created_at DESC').page(params[:page]).per(6)
+    end
+
+    def right_aside
+      @bird_locations = Post.all.order('created_at DESC').limit(1)
     end
 
 end
