b4184d08e80003c7ce0ae7e7e2a496ffbc01b2fd
Tatsuji Haga <blueandaqualine@gmail.com>
2019-10-17 19:42:22 +0900

    Update config/deploy.rb, tasks/task.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,74 +1,74 @@
-# capistranoのバージョン固定
-lock '3.11.1'
+# # capistranoのバージョン固定
+# lock '3.11.1'
 
-# デプロイするアプリケーション名
-set :application, 'toriposter'
+# # デプロイするアプリケーション名
+# set :application, 'toriposter'
 
-# cloneするgitのレポジトリ
-set :repo_url, 'git@github.com:t330/toriposter.git'
+# # cloneするgitのレポジトリ
+# set :repo_url, 'git@github.com:t330/toriposter.git'
 
-# deployするブランチ。デフォルトはmasterなのでなくても可。
-set :branch, 'master'
+# # deployするブランチ。デフォルトはmasterなのでなくても可。
+# set :branch, 'master'
 
-# deploy先のディレクトリ。 
-set :deploy_to, '/var/www/rails/toriposter'
+# # deploy先のディレクトリ。 
+# set :deploy_to, '/var/www/rails/toriposter'
 
-# シンボリックリンクをはるファイル。(※後述)
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+# # シンボリックリンクをはるファイル。(※後述)
+# set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
 
-# master.keyの保存場所？
-set :linked_files, fetch(:linked_files, []).push('config/master.key')
+# # master.keyの保存場所？
+# set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
-# シンボリックリンクをはるフォルダ。(※後述)
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# # シンボリックリンクをはるフォルダ。(※後述)
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# 保持するバージョンの個数(※後述)
-set :keep_releases, 5
+# # 保持するバージョンの個数(※後述)
+# set :keep_releases, 5
 
-# rubyのバージョン
-set :rbenv_ruby, '2.5.1'
+# # rubyのバージョン
+# set :rbenv_ruby, '2.5.1'
 
-#出力するログのレベル。
-set :log_level, :debug
+# #出力するログのレベル。
+# set :log_level, :debug
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicron.pid" }
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicron.pid" }
 
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
-  end
+# namespace :deploy do
+#   desc 'Restart application'
+#   task :restart do
+#     invoke 'unicorn:stop'
+#     invoke 'unicorn:start'
+#   end
 
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
-    end
-  end
+#   desc 'Create database'
+#   task :db_create do
+#     on roles(:db) do |host|
+#       with rails_env: fetch(:rails_env) do
+#         within current_path do
+#           execute :bundle, :exec, :rake, 'db:create'
+#         end
+#       end
+#     end
+#   end
 
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
+#   desc 'Run seed'
+#   task :seed do
+#     on roles(:app) do
+#       with rails_env: fetch(:rails_env) do
+#         within current_path do
+#           execute :bundle, :exec, :rake, 'db:seed'
+#         end
+#       end
+#     end
+#   end
 
-  after :publishing, :restart
+#   after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-    end
-  end
-end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#     end
+#   end
+# end
 
 # The code below is a default description, turned into a comment by me.
 
