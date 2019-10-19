8e452991c859f0102cc269a0379f9e8cfffe898a
Tatsuji Haga <blueandaqualine@gmail.com>
2019-10-20 03:03:30 +0900

    Update right_aside_html.haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,18 +14,18 @@ class ApplicationController < ActionController::Base
       @bird = Post.where('name LIKE(?)', "%#{params[:bird]}%").order('created_at DESC').page(params[:page]).per(6)
     end
 
-    def right_aside
-      @bird_locations = Post.all.order('created_at DESC').limit(5)
-      @location1 = @bird_locations[0]
-      @location1 = @bird_locations[0]
-      @location2 = @bird_locations[1]
-      @location2 = @bird_locations[1]
-      @location3 = @bird_locations[2]
-      @location3 = @bird_locations[2]
-      @location4 = @bird_locations[3]
-      @location4 = @bird_locations[3]
-      @location5 = @bird_locations[4]
-      @location5 = @bird_locations[4]
-    end
+    #def right_aside
+    #  @bird_locations = Post.all.order('created_at DESC').limit(5)
+    #  @location1 = @bird_locations[0]
+    #  @location1 = @bird_locations[0]
+    #  @location2 = @bird_locations[1]
+    #  @location2 = @bird_locations[1]
+    #  @location3 = @bird_locations[2]
+    #  @location3 = @bird_locations[2]
+    #  @location4 = @bird_locations[3]
+    #  @location4 = @bird_locations[3]
+    #  @location5 = @bird_locations[4]
+    #  @location5 = @bird_locations[4]
+    #end
 
 end
