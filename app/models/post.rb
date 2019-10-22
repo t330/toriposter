65b4d1b17a86c3d84f77eae83d9a1d5a97b53443
Tatsuji Haga <blueandaqualine@gmail.com>
2019-10-23 01:17:26 +0900

    Update submission functions

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :bird_list
   belongs_to :user
-  has_one :map, inverse_of: :post
+  has_one :map, inverse_of: :post, dependent: :destroy
   accepts_nested_attributes_for :map
   has_many :comments, dependent: :destroy
   
